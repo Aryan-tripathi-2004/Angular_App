a3d09ea5a04093877c182ef6aa1413b92c958141
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-22 14:18:23 +0530

    [Om Datt Tripathi] Add. Ability To launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularApp';
   imageUrl = '../assets/BL_logo_square_png.png';
+  url = 'https://www.bridgelabz.com/';
   
   ngOnInit() {
     console.log('App Component');
     this.title = 'Hello From Bridgelabz';
+
+  }
+
+  onClick($event: MouseEvent) {
+    console.log('Button Clicked',$event);
+    window.open(this.url, "_blank");
   }
 }
