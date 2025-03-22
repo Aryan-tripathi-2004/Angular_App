49c351506ceb52f2327b90de922a36c0f30ee760
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-22 15:00:16 +0530

    [Om Datt Tripathi] Add. validate userName

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imageUrl = '../assets/BL_logo_square_png.png';
   url = 'https://www.bridgelabz.com/';
   userName = '';
+  nameError = '';
   
   ngOnInit() {
     console.log('App Component');
@@ -20,5 +21,15 @@ export class AppComponent {
   onClick($event: MouseEvent) {
     console.log('Button Clicked',$event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event: Event) {
+    console.log('Input Event',$event);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = '';
+      return;
+    }
+    this.nameError = 'Name is Incorrect';
   }
 }
